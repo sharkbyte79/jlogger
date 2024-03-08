d5d47e00ac1a360b879261e4057e8bdc5c725a0b
Layne Pierce <ppierce273@gmail.com>
2024-03-07 22:00:20 -0500

    log now adds message logs to a Logger's logs field, will be logged concurrently upon calling FlushAll

--- jlogger.go
+++ jlogger.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"sync"
 
-	//"fmt"
 	"io"
-	//"os"
 	"time"
 )
 
@@ -22,6 +22,18 @@ const (
 	levelCrit
 )
 
+func main() {
+	f, _ := os.Create("Example.json")
+	w := io.Writer(f)
+
+	log := NewLogger(levelDebug, w)
+	for i := 0; i < 10000; i++ {
+		log.Warn(fmt.Sprintf("Log %d", i))
+	}
+
+	log.FlushAll()
+}
+
 // A Log represents a message log and it's properties.
 type Log struct {
 	Prefix  string `json:"prefix"`
@@ -30,24 +42,12 @@ type Log struct {
 	Time    string `json:"time"`
 }
 
-func main() {
-	f, _ := os.Create("Example.json")
-	w := io.Writer(f)
-
-	log := NewLogger(levelDebug, w)
-
-	log.Debug("This is a Debug level log.")
-	log.Info("This is a Info level log")
-	log.Warn("This is Warn level log.")
-	log.Error("This is an Error level log.")
-	log.Crit("This is a Crit level log.")
-}
-
 // A Logger writes log messages to a log destination in json format.
 type Logger struct {
-	threshold    int // threshold represents the lower bound for log levels that will be logged.
-	out          io.Writer
-	customLevels map[string]int
+	threshold int // threshold represents the lower bound for log levels that will be logged.
+	out       io.Writer
+	// customLevels map[string]int
+	logs map[int]Log
 }
 
 // NewLogger returns a new Logger.
@@ -55,16 +55,17 @@ func NewLogger(threshold int, out io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
 		out:       out,
+		logs:      make(map[int]Log),
 	}
 }
 
 // NewLog returns a new Log.
-func newLog(prefix string, level int, message string, time string) *Log {
-	return &Log{
+func newLog(prefix string, level int, message string) Log {
+	return Log{
 		Prefix:  prefix,
 		Level:   level,
 		Message: message,
-		Time:    time,
+		Time:    time.Now().String(),
 	}
 }
 
@@ -90,28 +91,34 @@ func (l Logger) levelPrefix(logLevel int) string {
 }
 
 // log writes a formatted log of level logLevel to a Logger's out.
-func (l Logger) log(logLevel int, message string) {
-	// Don't write to log file if the level of the log type
-	// in question is less than the logLevel threshold.
-	if l.threshold > logLevel {
-		return
-	}
-
-	// Get the timestamp as soon as the log level is
-	// found to pass the threshold.
-	timeStamp := time.Now().String()
-
+func (l *Logger) log(logLevel int, message string) {
 	// Get the appropriate prefix for the log message.
 	prefix := l.levelPrefix(logLevel)
 
-	log := newLog(prefix, logLevel, message, timeStamp)
-
-	log.writeJSON(l.out)
+	log := newLog(prefix, logLevel, message)
+	l.logs[len(l.logs)] = log
 }
 
-func (l *Log) writeJSON(w io.Writer) {
-	b := json.NewEncoder(w)
+// writeJSON encodes a log in json format and writes it to out.
+func (l *Log) writeJSON(w io.Writer, b json.Encoder) {
 	b.Encode(l)
+}
+
+// FlushAll encodes all of Logger's stored logs to JSON at the target out.
+func (l Logger) FlushAll() {
+	b := json.NewEncoder(l.out)
+	var wg sync.WaitGroup
+
+	for _, log := range l.logs {
+		wg.Add(1)
+		go func(currentLog Log) {
+			defer wg.Done()
+			currentLog.writeJSON(l.out, *b)
+		}(log)
+	}
+
+	wg.Wait()
+	fmt.Println("All logged!")
 }
 
 // Debug writes a Debug level message to a Logger's out.
@@ -161,11 +168,3 @@ func (l Logger) Crit(message string) {
 
 // Other writes a custom log level to a Logger's out.
 func (l Logger) Other() {}
-
-// addLogLevel adds a custom log level to a Logger's customLogs map, in the format
-// [logLevel]logName.
-// Also adds the custom log level to a Logger's reverseCustomLogs, in the format [logName]logLevel.
-
-// Assign custom log level to unique position in the Logger's hash map.
-// The logLevel is the key, so as to prevent duplicate log levels of the same severity
-// and keep it interacting well with levelPrefix().
